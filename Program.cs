1ceb5373204c7360d68f9912f0e56d9bddeab158
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-18 11:05:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YksiNumerot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tulostaa numerot 1, 11, 101, 1001 omille riveilleen
+            Console.WriteLine("1");
+            Console.WriteLine("11");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+
+            // Pysäytä ohjelma odottamaan syötettä ennen sulkemista
+            Console.ReadLine();
+        }
+    }
+}
